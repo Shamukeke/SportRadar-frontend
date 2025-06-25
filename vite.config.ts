9f12fca39a7b9285a003c990a4540a5e759fff58
Joa <joa.shamu@yahoo.fr>
2025-06-25 14:54:02 +0200

    correction fixe de vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/SportRadar-frontend/',
   resolve: {
     alias: {
       // Ajoutez ces alias pour les imports courants
