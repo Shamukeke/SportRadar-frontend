5b5edf141ae0d739b8682178c977bc9b79323d0d
Joa <joa.shamu@yahoo.fr>
2025-07-19 05:47:52 +0200

    axiosInstance

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -12,7 +12,7 @@ const PUBLIC_ENDPOINTS = [
 ];
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   headers: { 'Content-Type': 'application/json' },
 });
 
